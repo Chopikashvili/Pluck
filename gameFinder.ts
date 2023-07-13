ff75a37c4eb17de32146a300a5f40dab26cd1a17
David Chopikashvili <chopikashvilid@gmail.com>
2023-07-13 16:15:17 +0500

    Make constructGames callable for any season and add weather property

--- gameFinder.ts
+++ gameFinder.ts
@@ -5,7 +5,8 @@ export type GameInfo = {
     day: number,
     round: number,
     awayTeam: string,
-    homeTeam: string
+    homeTeam: string,
+    weather: string | null
 };
 
 export type Games = {
@@ -16,12 +17,12 @@ export type Games = {
  * Parses a season of Hlockey and constructs a games object.
  * @returns the games object inside a promise.
  */
-export async function constructGames(): Promise<Games> {
+export async function constructGames(season: number): Promise<Games> {
     let games: Games = {};
     let basePath: string;
     for (let i = 1; i <= 38; i++) {
         for (let j = 1; j <= 3; j++) {
-            basePath = `../Downloads/hlockeyarchive3/Season 2/Day ${i < 10 ? ('0' + i.toString()) : i.toString()}/Round ${j.toString()}`;
+            basePath = `../Downloads/hlockeyarchive3/Season ${season.toString()}/Day ${i < 10 ? ('0' + i.toString()) : i.toString()}/Round ${j.toString()}`;
             let files: string[];
             try {
                 files = await fs.readdir(basePath); //reads every game from the round
@@ -32,12 +33,14 @@ export async function constructGames(): Promise<Games> {
             let k = 0;
             for (let game of files) {
                 let gameAddress = `d${i.toString()}r${j.toString()}g${(k + 1).toString()}`;
+                let info = await fs.readFile(basePath + '/' + game + '/info.txt', 'utf-8');
                 games[gameAddress] = {
                     path: basePath + '/' + game,
                     day: i,
                     round: j,
                     awayTeam: game.substring(0, game.indexOf('vs') - 1),
-                    homeTeam: game.substring(game.indexOf('vs') + 3)
+                    homeTeam: game.substring(game.indexOf('vs') + 3),
+                    weather: season >= 3 ? info.substring(info.lastIndexOf(':') + 2, info.length - 1) : null
                 }; //adds info about games to the games object
                 k++;
             }
@@ -49,14 +52,14 @@ export async function constructGames(): Promise<Games> {
  * Logs the first game of constructGames
  */
 async function logger(): Promise<void> {
-    const games: Games = await constructGames();
+    const games: Games = await constructGames(3);
     console.log(games['d1r1g1']);
 }
 /**
  * Writes the result of constructGames to a JSON file
  */
 async function writer(): Promise<void> {
-    fs.writeFile('json/season2games.json', JSON.stringify(await constructGames()), 'utf-8');
+    fs.writeFile('json/season3games.json', JSON.stringify(await constructGames(3)));
 }
 
 writer();
