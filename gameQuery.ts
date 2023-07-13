6cdd6c9345b217348a8ae76ddba4d42135397f83
David Chopikashvili <chopikashvilid@gmail.com>
2023-07-13 19:03:33 +0500

    Add function to query games

---
+++ gameQuery.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs/promises';
+import { Games } from './gameFinder';
+
+async function gameQuery(season: number, key: string, value: string): Promise<void> {
+    let result: any[] = [];
+    const data: string = await fs.readFile(`json/season${season.toString()}games.json`, 'utf-8');
+    const parsedData: Games = JSON.parse(data); //parses JSON
+    if (typeof parsedData['d1r1g1'][key] == undefined) console.log('No such stat.');
+    else {
+        for (let game of Object.values(parsedData)) {
+            if (game[key] == value) result.push(game);
+        }
+        console.log(result);
+    }
+}
+
+gameQuery(3, 'weather', 'Waves');
